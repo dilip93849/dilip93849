b742f3b2040a16f92d9b51cbdbd1552b40a3aad5
dilip93849 <142787849+dilip93849@users.noreply.github.com>
2024-09-21 23:03:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 function toggleMode() {
-    const body = document.body;
-    const button = document.querySelector('button');
-    body.classList.toggle('dark-mode');
-    if (body.classList.contains('dark-mode')) {
-        button.textContent = 'Switch to Light Mode';
-    } else {
-        button.textContent = 'Switch to Dark Mode';
-    }
+    document.body.classList.toggle('dark-mode');
 }
